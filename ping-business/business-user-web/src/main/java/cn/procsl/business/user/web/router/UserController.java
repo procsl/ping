54626a1e5ca527473909530a6feee673d0a61863
procsl <program_chen@foxmail.com>
2020-01-03 16:30:34 +0800

    修改配置

--- ping-business/business-user-web/src/main/java/cn/procsl/business/user/web/router/UserController.java
+++ ping-business/business-user-web/src/main/java/cn/procsl/business/user/web/router/UserController.java
@@ -1,8 +1,11 @@
 package cn.procsl.business.user.web.router;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Stream;
 
 /**
  * @author procsl
@@ -13,9 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @PostMapping
-    public void create() {
+    public String create() {
+        return "hello";
     }
 
+    @GetMapping
     public String test() {
         return "test";
     }
