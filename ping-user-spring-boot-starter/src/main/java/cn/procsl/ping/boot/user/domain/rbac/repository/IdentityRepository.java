94c7eb338f0442ad14a41e1881de96846ee15a75
csl <program.chen@foxmail.com>
2020-05-18 23:53:05 +0800

    优化导入

--- ping-user-spring-boot-starter/src/main/java/cn/procsl/ping/boot/user/domain/rbac/repository/IdentityRepository.java
+++ ping-user-spring-boot-starter/src/main/java/cn/procsl/ping/boot/user/domain/rbac/repository/IdentityRepository.java
@@ -3,9 +3,6 @@ package cn.procsl.ping.boot.user.domain.rbac.repository;
 import cn.procsl.ping.boot.user.domain.rbac.model.Identity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.stereotype.Repository;
-
-import javax.persistence.ColumnResult;
 
 /**
  * @author procsl
