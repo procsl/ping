a66b8c3a4c7816c48b3654be7c3c56c1093bc46a
csl <program.chen@foxmail.com>
2020-07-08 19:11:16 +0800

    重写单元测试

--- ping-user-spring-boot-starter/src/test/java/cn/procsl/ping/boot/user/domain/rbac/entity/SessionTest.java
+++ ping-user-spring-boot-starter/src/test/java/cn/procsl/ping/boot/user/domain/rbac/entity/SessionTest.java
@@ -2,132 +2,111 @@ package cn.procsl.ping.boot.user.domain.rbac.entity;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.Before;
+import org.junit.Test;
 
-import javax.inject.Inject;
-import java.util.Optional;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.Collections.EMPTY_SET;
 
 /**
  * @author procsl
- * @date 2020/04/28
+ * @date 2020/07/08
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Transactional
 @Slf4j
-class SessionTest {
+public class SessionTest {
 
-    @Inject
-    JpaRepository<Session, Long> sessionJpaRepository;
+    private Set<Long> roles;
+    private Session sesssion0;
+    private Session sesssion1;
 
-    @Test
-    void testAddRole() {
-        Session session = Session.creator().active(true).done();
-        Long id = sessionJpaRepository.save(session).getId();
-
-        Optional<Session> session1 = sessionJpaRepository.findById(id);
-
-        session1.get().addRole(1L);
-        log.info("{}", session1.get());
-        sessionJpaRepository.flush();
+    @Before
+    public void before() {
+        this.sesssion0 = new Session(true);
+        this.roles = Arrays.asList(1L, 2L, 3L).stream().collect(Collectors.toSet());
+        this.sesssion1 = new Session(false, roles);
     }
 
     @Test
-    void testRemove() {
-        Session session = Session.creator().active(true).done();
-        session.addRole(1L);
-        session.addRole(2L);
-        session.addRole(3L);
-        session.addRole(4L);
-        session.addRole(5L);
-        session.addRole(6L);
-        session.addRole(7L);
-        session.addRole(8L);
-        Long id = sessionJpaRepository.save(session).getId();
-        sessionJpaRepository.flush();
+    public void init() {
+        new Session(true);
 
+        new Session(true, null);
 
-        Optional<Session> tmp = sessionJpaRepository.findById(id);
-        tmp.ifPresent(s -> {
-            s.remove(1L);
-            s.remove(2L);
-            s.remove(3L);
-        });
-        Assert.assertNotNull(tmp.get());
-        sessionJpaRepository.saveAndFlush(session);
+        new Session(false, null);
 
-        Optional<Session> tmp2 = sessionJpaRepository.findById(id);
-        tmp2.ifPresent(s -> {
-            boolean bool1 = s.getRoles().contains(1L);
-            Assert.assertFalse(bool1);
+        new Session(false, EMPTY_SET);
 
+        new Session(true, Collections.singleton(1L));
 
-            boolean bool2 = s.getRoles().contains(2L);
-            Assert.assertFalse(bool2);
+        new Session(true, roles);
+    }
 
-
-            boolean bool3 = s.getRoles().contains(3L);
-            Assert.assertFalse(bool3);
-
-
-            boolean bool4 = s.getRoles().contains(4L);
-            Assert.assertTrue(bool4);
-        });
-        Assert.assertNotNull(tmp2.get());
+    @Test(expected = NullPointerException.class)
+    public void addRole() {
+        this.sesssion0.addRole(null);
     }
 
     @Test
-    void testHasRole() {
-        Session session = Session.creator().active(true).done();
-        session.addRole(1L);
-        Long id = sessionJpaRepository.save(session).getId();
-        sessionJpaRepository.flush();
-
-        Optional<Session> tmp = sessionJpaRepository.findById(id);
-        Assert.assertNotNull(tmp.get());
-
-        boolean bool = tmp.get().hasRole(1L);
-        Assert.assertTrue(bool);
-
-        boolean bool2 = tmp.get().hasRole(2L);
-        Assert.assertFalse(bool2);
-    }
-
-
-    @Test
-    void enable() {
-        Session session = Session.creator().active(false).done();
-        Long id = sessionJpaRepository.save(session).getId();
-        sessionJpaRepository.flush();
-
-        Session session1 = sessionJpaRepository.findById(id).get();
-        Assert.assertFalse(session1.isActive());
-
-        session.enable();
-        sessionJpaRepository.save(session);
-
-        Session session2 = sessionJpaRepository.findById(id).get();
-        Assert.assertTrue(session2.isActive());
+    public void addRoleCase1() {
+        this.sesssion0.addRole(1L);
     }
 
     @Test
-    void disable() {
-        Session session = Session.creator().active(false).done();
-        Long id = sessionJpaRepository.save(session).getId();
-        sessionJpaRepository.flush();
+    public void addRoleCase2() {
+        this.sesssion0.addRole(-1L);
+    }
 
-        Session session1 = sessionJpaRepository.findById(id).get();
-        Assert.assertFalse(session1.isActive());
+    @Test(expected = NullPointerException.class)
+    public void remove() {
+        this.sesssion0.remove(null);
+    }
 
-        session.disable();
-        sessionJpaRepository.save(session);
+    @Test
+    public void removeCase1() {
+        this.sesssion0.remove(1L);
+    }
 
-        Session session2 = sessionJpaRepository.findById(id).get();
-        Assert.assertFalse(session2.isActive());
+    @Test
+    public void removeCase2() {
+        this.sesssion0.remove(-1L);
+    }
+
+    @Test
+    public void hasRole() {
+        Assert.assertFalse(this.sesssion0.hasRole(0L));
+        Assert.assertFalse(this.sesssion0.hasRole(1L));
+        Assert.assertFalse(this.sesssion0.hasRole(2L));
+
+        Assert.assertTrue(this.sesssion1.hasRole(1L));
+        Assert.assertTrue(this.sesssion1.hasRole(2L));
+        Assert.assertTrue(this.sesssion1.hasRole(3L));
+    }
+
+    @Test
+    public void enable() {
+        Assert.assertTrue(this.sesssion0.isActive());
+
+        sesssion0.enable();
+        Assert.assertTrue(this.sesssion0.isActive());
+
+        Assert.assertFalse(this.sesssion1.isActive());
+        sesssion1.enable();
+        Assert.assertTrue(this.sesssion1.isActive());
+    }
+
+    @Test
+    public void disable() {
+        Assert.assertTrue(this.sesssion0.isActive());
+
+        sesssion0.disable();
+        Assert.assertFalse(this.sesssion0.isActive());
+
+        Assert.assertFalse(this.sesssion1.isActive());
+        sesssion1.disable();
+        Assert.assertFalse(this.sesssion1.isActive());
     }
 }
