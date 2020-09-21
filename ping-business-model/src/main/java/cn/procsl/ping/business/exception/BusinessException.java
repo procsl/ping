11ac4a8fc730687346ba1b9d967e3a0ed0215b18
procsl <program_chen@foxmail.com>
2020-09-21 17:06:22 +0800

    添加异常操作方法
    Signed-off-by: procsl <program_chen@foxmail.com>

--- ping-business-model/src/main/java/cn/procsl/ping/business/exception/BusinessException.java
+++ ping-business-model/src/main/java/cn/procsl/ping/business/exception/BusinessException.java
@@ -1,5 +1,7 @@
 package cn.procsl.ping.business.exception;
 
+import lombok.NonNull;
+
 /**
  * 业务异常
  *
@@ -8,8 +10,23 @@ package cn.procsl.ping.business.exception;
  */
 public class BusinessException extends RuntimeException {
 
+    final static String MESSAGE_TEMPLATE = "[%s] not found!";
+
     public BusinessException(String format, Object... arguments) {
         // TODO 这里应该用占位符的, 先写着
         super(format);
+    }
+
+    public static <T> T ifNotFound(T entity, @NonNull String message) {
+        if (entity == null) {
+            throw new IllegalArgumentException(String.format(MESSAGE_TEMPLATE, message));
+        }
+        return entity;
+    }
+
+    public static <T> void ifNotFoundOnlyThrow(T entity, @NonNull String message) {
+        if (entity == null) {
+            throw new IllegalArgumentException(String.format(MESSAGE_TEMPLATE, message));
+        }
     }
 }
