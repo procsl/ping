57975d631ecb66daf5f942ff6c34051dde7ba638
procsl <program_chen@foxmail.com>
2021-07-05 17:48:10 +0800

    优化导入 添加注解处理器

--- ping-processor-api/src/main/java/cn/procsl/ping/processor/builder/RequestParamAnnotationBuilder.java
+++ ping-processor-api/src/main/java/cn/procsl/ping/processor/builder/RequestParamAnnotationBuilder.java
@@ -3,18 +3,30 @@ package cn.procsl.ping.processor.builder;
 
 import cn.procsl.ping.processor.ProcessorContext;
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.ParameterSpec;
 
-import javax.annotation.Nullable;
 import javax.lang.model.element.Element;
 
 
 @AutoService(AnnotationSpecBuilder.class)
-public class RequestParamAnnotationBuilder implements AnnotationSpecBuilder {
+public class RequestParamAnnotationBuilder extends AbstractAnnotationSpecBuilder<ParameterSpec.Builder> {
 
     final static String requestParam = "org.springframework.web.bind.annotation.RequestParam";
 
+
     @Override
-    public <T extends Element> void build(ProcessorContext context, @Nullable T source, Object target, String type) {
+    protected boolean isType(String type) {
+        return "CONTROLLER".equals(type);
+    }
+
+    @Override
+    protected <E extends Element> void buildTargetAnnotation(ProcessorContext context, E source, ParameterSpec.Builder target) {
 
     }
+
+    @Override
+    protected Class<ParameterSpec.Builder> target() {
+        return ParameterSpec.Builder.class;
+    }
+
 }
