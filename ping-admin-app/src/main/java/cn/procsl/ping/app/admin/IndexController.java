73ddafe7f2937be04657a5be36c9a0c0f7fed059
procsl <program_chen@foxmail.com>
2021-07-08 00:03:03 +0800

    添加Spring doc注解测试代码

--- ping-admin-app/src/main/java/cn/procsl/ping/app/admin/IndexController.java
+++ ping-admin-app/src/main/java/cn/procsl/ping/app/admin/IndexController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
@@ -19,11 +20,11 @@ import java.util.Map;
 @Tag(name = "这是Tag", description = "这是tag.description")
 public class IndexController {
 
-    @GetMapping(path = {"index"})
+    @GetMapping(path = {"index/{id}"})
 //    @RouterOperation(operation = @Operation(operationId = "index", summary = "这是摘要", description = "这是描述"))
     @ApiResponse(responseCode = "200", description = "这是ApiResponse描述", content = @Content())
-    @Parameter(name = "id", description = "这是参数描述", in = ParameterIn.QUERY)
-    public Map<String, String> index(String id) {
+    @Deprecated
+    public Map<String, String> index(@Parameter(description = "这是参数描述", in = ParameterIn.PATH) @PathVariable("id") String id) {
         return Map.of("root", "123456");
     }
 
