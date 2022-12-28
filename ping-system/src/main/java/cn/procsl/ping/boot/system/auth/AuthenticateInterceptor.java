3bb87daf550d2b0d430f62be281fbe4c4f875d1a
procsl <program_chen@foxmail.com>
2022-12-28 10:04:08 +0800

    修复接口校验
    Signed-off-by: procsl <program_chen@foxmail.com>

--- ping-system/src/main/java/cn/procsl/ping/boot/system/auth/AuthenticateInterceptor.java
+++ ping-system/src/main/java/cn/procsl/ping/boot/system/auth/AuthenticateInterceptor.java
@@ -5,11 +5,17 @@ import cn.procsl.ping.boot.system.domain.user.AuthenticateException;
 import jakarta.annotation.security.PermitAll;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.server.PathContainer;
 import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.util.pattern.PathPattern;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 import static cn.procsl.ping.boot.system.auth.SessionController.session_key;
 
 public class AuthenticateInterceptor extends AbstractMethodAnnotationInterceptor<PermitAll> {
+
+    PathPattern pattern = PathPatternParser.defaultInstance.parse("/v1/**");
+
 
     public AuthenticateInterceptor() {
         super(PermitAll.class);
@@ -22,6 +28,13 @@ public class AuthenticateInterceptor extends AbstractMethodAnnotationInterceptor
         if (permit) {
             return true;
         }
+
+        String uri = request.getRequestURI();
+        PathContainer pathContainer = PathContainer.parsePath(uri);
+        if (!pattern.matches(pathContainer)) {
+            return true;
+        }
+
         Object id = request.getSession().getAttribute(session_key);
         if (id != null) {
             return true;
