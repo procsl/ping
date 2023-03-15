9c4ba878344d9bb1f36e1fea98ed7c2ea0d55a96
chenshanlu <program_chen@foxmail.com>
2023-03-15 16:08:43 +0800

    添加测试文件

---
+++ ping-product/src/main/java/cn/procsl/ping/boot/product/service/ProductService.java
@@ -0,0 +1,18 @@
+package cn.procsl.ping.boot.product.service;
+
+import org.springframework.stereotype.Indexed;
+import org.springframework.stereotype.Service;
+
+@Indexed
+@Service
+public class ProductService {
+
+
+
+
+    public void productCreator() {
+
+    }
+
+
+}
